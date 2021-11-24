c58d5b1038784ac14a621d190135954e098f194c
DavidNathaniel <91140843+DavidNathaniel@users.noreply.github.com>
2021-11-24 11:45:48 +0000

    added contacts file

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express();
 //make the app listen on port
 //send an HTTP response when receiving HTTP GET /
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
 });
 
 const port = process.argv[2] || process.env.PORT || 3000;
