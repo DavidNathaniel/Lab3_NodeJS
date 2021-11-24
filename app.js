147c5ddad9cb83d5b21a3cfaf61fb07cc20c6016
DavidNathaniel <91140843+DavidNathaniel@users.noreply.github.com>
2021-11-24 18:11:04 +0000

    forgot to add login and register pages will need to add the forms to make it look right, but the basic code is there

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get("/contacts", (req, res) => {
 app.get("/catalogue", (req, res) => {
   res.render("catalogue");
 });
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/article", (req, res) => {
+  res.render("article");
+});
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
